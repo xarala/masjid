6823423f5fc24c49aa96136365195532d00caa4c
Papa SENE <pathe.sene@xarala.sn>
2012-01-31 00:35:38 +0000

    added doc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Masjid::Application.config.secret_token = '4aa286afcb68a16de12361cb2fc3da42b6e1f19d8234a1752e24b468ed7a97601983d2e282abc507a9af4fa1b091eadecdebca7d533d2dfbad0f1aa3c1dac7b2'
